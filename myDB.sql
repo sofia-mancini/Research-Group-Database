879331044e7b6dad98ed6caefa2292132a4f2845
keelyfowler <keelyfowler@uri.edu>
2026-02-20 15:57:20 -0500

    Update schemas to match Phase 2 ER Diagram changes; Add relationship tables with foreign keys.

--- myDB.sql
+++ myDB.sql
@@ -1,24 +1,26 @@
 USE sofiaman_research_db;
 
 -- --------------------------------------------------------
--- Table structure for table `Person`
+-- Table structure for table `person`
 -- --------------------------------------------------------
-CREATE TABLE `Person` (
-  `ID`    int(100)     NOT NULL AUTO_INCREMENT,
-  `name`  varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `role`  varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`ID`)
+CREATE TABLE `person` (
+
+  `person_id` int(100)     NOT NULL AUTO_INCREMENT,
+
+  `name`      varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+
+  `email`     varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+
+  PRIMARY KEY (`person_id`)
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 -- --------------------------------------------------------
-
--- Table structure for table `Department`
-
+-- Table structure for table `department`
 -- --------------------------------------------------------
 
-CREATE TABLE `Department` (
+CREATE TABLE `department` (
 
 `department_id` int(100) NOT NULL AUTO_INCREMENT,
 
@@ -30,13 +32,12 @@ PRIMARY KEY (`department_id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
--- Table structure for table `Research_Group`
 
 -- --------------------------------------------------------
+-- Table structure for table `research_group`
+-- --------------------------------------------------------
 
-CREATE TABLE `Research_Group` (
+CREATE TABLE `research_group` (
 
 `group_id` int(100) NOT NULL AUTO_INCREMENT,
 
@@ -48,13 +49,12 @@ PRIMARY KEY (`group_id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
--- Table structure for table `Project`
 
 -- --------------------------------------------------------
+-- Table structure for table `project`
+-- --------------------------------------------------------
 
-CREATE TABLE `Project` (
+CREATE TABLE `project` (
 
 `project_id`  int(100)     NOT NULL AUTO_INCREMENT,
 
@@ -72,19 +72,20 @@ PRIMARY KEY (`project_id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
--- Table structure for table `Experiment`
 
 -- --------------------------------------------------------
+-- Table structure for table `experiment`
+-- --------------------------------------------------------
 
-CREATE TABLE `Experiment` (
+CREATE TABLE `experiment` (
 
 `experiment_id` int(100)     NOT NULL AUTO_INCREMENT,
 
 `title`         varchar(100) COLLATE utf8_unicode_ci NOT NULL,
 
-`theory`        text         COLLATE utf8_unicode_ci,
+`objective`     text         COLLATE utf8_unicode_ci,
+
+`description`   text         COLLATE utf8_unicode_ci,
 
 `start_date`    date,
 
@@ -96,19 +97,18 @@ PRIMARY KEY (`experiment_id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
--- Table structure for table `Task`
 
 -- --------------------------------------------------------
+-- Table structure for table `task`
+-- --------------------------------------------------------
 
-CREATE TABLE `Task` (
+CREATE TABLE `task` (
 
 `task_id`        int(100)     NOT NULL AUTO_INCREMENT,
 
 `title`          varchar(100) COLLATE utf8_unicode_ci NOT NULL,
 
-`theory`         text         COLLATE utf8_unicode_ci,
+`description`    text         COLLATE utf8_unicode_ci,
 
 `status`         varchar(50)  COLLATE utf8_unicode_ci NOT NULL,
 
@@ -116,17 +116,18 @@ CREATE TABLE `Task` (
 
 `completed_date` date,
 
+`progress`       int(3)       DEFAULT 0,
+
 PRIMARY KEY (`task_id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
--- Table structure for table `Literature`
 
 -- --------------------------------------------------------
+-- Table structure for table `literature`
+-- --------------------------------------------------------
 
-CREATE TABLE `Literature` (
+CREATE TABLE `literature` (
 
 `lit_id`  int(100)     NOT NULL AUTO_INCREMENT,
 
@@ -142,6 +143,237 @@ CREATE TABLE `Literature` (
 
 `url`     varchar(255) COLLATE utf8_unicode_ci,
 
+`theory`  text         COLLATE utf8_unicode_ci,
+
 PRIMARY KEY (`lit_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+-- --------------------------------------------------------
+-- Table structure for relationship table `person_dept`
+-- Connects person to department (M:1)
+-- Many persons can belong to one department
+-- --------------------------------------------------------
+
+CREATE TABLE `person_dept` (
+
+  `person_id`     int(100) NOT NULL,
+
+  `department_id` int(100) NOT NULL,
+
+  PRIMARY KEY (`person_id`, `department_id`),
+
+  FOREIGN KEY (`person_id`)     REFERENCES `person`     (`person_id`),
+
+  FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Table structure for relationship table `research_dept`
+-- Connects research_dept to department (M:1)
+-- Many research groups can belong to one department
+-- --------------------------------------------------------
+
+CREATE TABLE `research_dept` (
+
+  `group_id` int(100) NOT NULL,
+
+  `department_id`    int(100) NOT NULL,
+
+  PRIMARY KEY (`group_id`, `department_id`),
+
+  FOREIGN KEY (`group_id`) REFERENCES `research_group` (`group_id`),
+
+  FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Table structure for relationship table `person_group`
+-- Connects person to research_group (M:M)
+-- A person can belong to many research groups
+-- A research group can have many people
+-- Includes role and join_date as relationship attributes
+-- --------------------------------------------------------
+
+CREATE TABLE `person_group` (
+
+  `person_id` int(100) NOT NULL,
+
+  `group_id` int(100) NOT NULL,
+
+  `role` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+
+  `join_date` date,
+
+  PRIMARY KEY (`person_id`, `group_id`),
+
+  FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
+
+  FOREIGN KEY (`group_id`) REFERENCES `research_group` (`group_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Relationship table `group_project`
+-- Connects research_group to project (1:M)
+-- One research group can have many projects     
+-- --------------------------------------------------------
+
+CREATE TABLE `group_project` (
+
+  `group_id` int(100) NOT NULL,
+
+  `project_id` int(100) NOT NULL,
+
+  PRIMARY KEY (`group_id`, `project_id`),
+
+  FOREIGN KEY (`group_id`) REFERENCES `research_group` (`group_id`),
+
+  FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Relationship table for  `project_member`
+-- Connects person to project (M:M)
+-- A person can be a member of many projects
+-- A project can have many persons
+-- Includes role and join_date as relationship attributes
+-- --------------------------------------------------------
+
+CREATE TABLE `project_member` (
+
+  `person_id` int(100) NOT NULL,
+
+  `project_id` int(100) NOT NULL,
+
+  `role` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+
+  `join_date` date,
+
+  PRIMARY KEY (`person_id`, `project_id`),
+  
+  FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
+
+  FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Relationship table for `project_tasks`
+-- Connects project to task (1:M)
+-- One project can have many tasks
+-- --------------------------------------------------------
+
+CREATE TABLE `project_tasks` (
+
+  `project_id` int(100) NOT NULL,
+
+  `task_id` int(100) NOT NULL,
+
+  PRIMARY KEY (`project_id`, `task_id`),
+
+  FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`),
+
+  FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Relationship table for `task_assignment`
+-- Connects person to task (M:M)
+-- A person can be assigned to many tasks
+-- A task can be assigned to many persons
+-- Includes assigned_date as a relationship attribute
+-- --------------------------------------------------------
+
+CREATE TABLE `task_assignment` (
+
+  `person_id`     int(100) NOT NULL,
+
+  `task_id`       int(100) NOT NULL,
+
+  `assigned_date` date,
+
+  PRIMARY KEY (`person_id`, `task_id`),
+
+  FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
+
+  FOREIGN KEY (`task_id`)   REFERENCES `task`   (`task_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Relationship table for `project_expt`
+-- Connects project to experiment (One-to-Many)
+-- One project can have many experiments
+-- --------------------------------------------------------
+
+CREATE TABLE `project_expt` (
+
+  `project_id`    int(100) NOT NULL,
+
+  `experiment_id` int(100) NOT NULL,
+
+  PRIMARY KEY (`project_id`, `experiment_id`),
+
+  FOREIGN KEY (`project_id`)    REFERENCES `project`    (`project_id`),
+
+  FOREIGN KEY (`experiment_id`) REFERENCES `experiment` (`experiment_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Relationship table for `project_literature`
+-- Connects project to literature (Many-to-Many)
+-- A project can have many literature references
+-- A literature entry can belong to many projects
+-- --------------------------------------------------------
+
+CREATE TABLE `project_literature` (
+
+  `project_id` int(100) NOT NULL,
+
+  `lit_id`     int(100) NOT NULL,
+
+  PRIMARY KEY (`project_id`, `lit_id`),
+
+  FOREIGN KEY (`project_id`) REFERENCES `project`    (`project_id`),
+
+  FOREIGN KEY (`lit_id`)     REFERENCES `literature` (`lit_id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+-- Relationship table for `expt_literature`
+-- Connects experiment to literature (Many-to-Many)
+-- An experiment can reference many literature entries
+-- and a literature entry can belong to many experiments
+-- --------------------------------------------------------
+
+CREATE TABLE `expt_literature` (
+
+  `experiment_id` int(100) NOT NULL,
+
+  `lit_id`        int(100) NOT NULL,
+
+  PRIMARY KEY (`experiment_id`, `lit_id`),
+
+  FOREIGN KEY (`experiment_id`) REFERENCES `experiment` (`experiment_id`),
+
+  FOREIGN KEY (`lit_id`)        REFERENCES `literature` (`lit_id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
